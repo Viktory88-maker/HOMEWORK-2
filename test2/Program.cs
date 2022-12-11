4ba4d66da2b7e6f2817502e7aa7bf5f8eaf42c77
Viktoria <v1k0808@yandex.ru>
2022-12-11 18:30:52 +0300

    test

---
+++ test2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу,которая выводит третью цифру
+// заданного числа или сообщает, что ретьей цифры нет
+
+Console.WriteLine("Введите число");
+int num = Math.Abs(int.Parse(Console.ReadLine()!));
+int numTemp = num;
+int count = 0;
+
+while(numTemp > 0)
+{
+    numTemp /=10;
+    count++;
+}
+if (count - 3 >=0)
+{
+    while (count-3 > 0)
+    {
+        num = num/10;
+        count--;
+    }
+    Console.WriteLine($"Третья цифра {num % 10}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
