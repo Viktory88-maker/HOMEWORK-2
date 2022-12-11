ae98f855349bed97e350c1c290d33069ea22b69e
Viktoria <v1k0808@yandex.ru>
2022-12-11 19:05:52 +0300

    test3

---
+++ test3/Program.cs
@@ -0,0 +1,19 @@
+﻿// напишите программу, которая принимает на вход
+// цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным
+
+Console.WriteLine ("Введите день недели...");
+int dayofweek = int.Parse (Console.ReadLine());
+
+if (dayofweek == 6 || dayofweek == 7) 
+{
+Console.WriteLine ("Ответ: Выходной день.");
+}
+else if (dayofweek <= 5) 
+{
+    Console.WriteLine ("Ответ: Будний день.");
+}
+else if (dayofweek > 7) 
+{
+    Console.WriteLine ("Ошибка: В неделе всего 7 дней!");		
+}
